cf99fa03607cb2a08ba755e09c75d8489b781d6b
Muzammilzia <muzzammilzia20@gmail.com>
2024-05-28 12:35:44 +0500

    removed unused variables

--- src/projects/simple-caesar-cipher.tsx
+++ src/projects/simple-caesar-cipher.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
 import { FC, useState } from "react";
 import { Encryption } from "../components/simple-caesar-cipher/encryption";
 import { Decryption } from "../components/simple-caesar-cipher/decryption";
